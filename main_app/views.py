019d63a91194c0080a9e7a261161c4782f826a0e
Sam Revanoor <sam@trint.com>
2021-06-12 18:41:05 -0400

    sets up template inheritance, index file for all flavours, adds hardcoded flavours model temporarily, adds css

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+class Flavour:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, rating):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.rating = rating
+
+flavours = [
+  Flavour('Half Baked', 'dairy', 'Chocolate & vanilla ice creams with fudge brownies & gobs of chocolate chip cookie dough', 5),
+  Flavour('Cherry Garcia', 'non-dairy', 'Cherry Non-Dairy Frozen Dessert with Cherries & Fudge Flakes', 2),
+  Flavour('Chocolate Fudge Brownie', 'dairy', 'This flavour combines our chocolate ice cream with chunks of chewy, fudgy brownies.', 4)
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def flavours_index(request):
+  return render(request, 'flavours/index.html', { 'flavours': flavours })
